7fe023355aa858a1aa96e8cbc551eb93c9c2aeda
Giora Guttsait <giora111@gmail.com>
2018-08-04 00:13:05 +0300

    Add GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float restartDelay = 3f;
+    public event EventHandler GameEnded;
+
+    private bool gameHasEnded = false;
+
+    public static GameManager GetGameManager()
+    {
+        return FindObjectOfType<GameManager>();
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded)
+        {
+            return;
+        }
+
+        Debug.Log("GAME OVER!!");
+        gameHasEnded = true;
+        GameEnded.Invoke(null, null);
+        Invoke("RestartGame", restartDelay);
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
