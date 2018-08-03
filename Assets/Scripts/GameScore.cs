5a49c9dc210b35bea23c08f631b1729dfccd43a5
Giora Guttsait <giora111@gmail.com>
2018-08-04 00:17:50 +0300

    Add Game Score

---
+++ Assets/Scripts/GameScore.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScore : MonoBehaviour
+{
+    public PlayerMovement player;
+    public Text scoreText;
+    private string lastScore;
+
+    private void Start()
+    {
+        GameManager.GetGameManager().GameEnded += OnGameEnded;
+    }
+
+    private void OnGameEnded(object sender, System.EventArgs e)
+    {
+        scoreText.text = System.String.Format("Game Over! Score: {0}", lastScore);
+    }
+
+    private void Update()
+    {
+        if (player.enabled)
+        {
+            lastScore = CurrentPlayerZ();
+            scoreText.text = lastScore;
+        }
+    }
+
+    private string CurrentPlayerZ()
+    {
+        return player.transform.position.z.ToString("0");
+    }
+}
