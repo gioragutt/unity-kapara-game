0c6cc401e574664ceec6cee91f3eed7a265bc033
Giora Guttsait <giora111@gmail.com>
2018-08-08 01:33:14 +0300

    Add keyboard shortcuts for the Credits scene buttons

---
+++ Assets/Scripts/CreditsKeyboardShortcuts.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsKeyboardShortcuts : MonoBehaviour
+{
+    public Credits credits;
+    public Text restartButtonText;
+    public Text quitButtonText;
+    public KeyCode restartKey = KeyCode.Space;
+    public KeyCode quitKey = KeyCode.Escape;
+
+    private void Start()
+    {
+        AppendShortcutHelperToButton(restartButtonText, restartKey);
+        AppendShortcutHelperToButton(quitButtonText, quitKey);
+    }
+
+    private static void AppendShortcutHelperToButton(Text text, KeyCode shortcut)
+    {
+        text.text += string.Format(" ({0})", shortcut);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(restartKey))
+            credits.Restart();
+        if (Input.GetKey(quitKey))
+            credits.Quit();
+    }
+}
