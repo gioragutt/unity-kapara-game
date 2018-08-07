833d716476f67d7a5680db8de8fc08b373a1ff08
Giora Guttsait <giora111@gmail.com>
2018-08-07 21:30:24 +0300

    Show Score in the Credits scene

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,7 +1,20 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
+    public Text scoreText;
+    public string scoreTextPrefix = "Score: ";
+
+    private void Start()
+    {
+        scoreText.text = string.Format(
+            "{0}{1}", 
+            scoreTextPrefix, 
+            GameData.Instance.score.ToIntegerString());
+    }
+
     public void Restart()
     {
         GameManager.Get().RestartGame();
