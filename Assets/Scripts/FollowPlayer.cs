f66f81598fafa65d39b8fd2bda72bec4eefecc92
Giora Guttsait <giora111@gmail.com>
2018-08-08 13:45:44 +0300

    Add smooth follow

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,9 +4,28 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
+    public bool isSmoothFollow = true;
+    public float smoothFactor = 10f;
 
-    void Update()
+    void FixedUpdate()
+    {
+        if (isSmoothFollow)
+            SmoothFollow();
+        else
+            NormalFollow();
+    }
+
+    private void NormalFollow()
     {
         transform.position = player.position + offset;
+    }
+
+    private void SmoothFollow()
+    {
+        var desiredPosition = player.position + offset;
+        var position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothFactor);
+        position.z = player.position.z + offset.z;
+        transform.position = position;
+        transform.LookAt(player);
     }
 }
