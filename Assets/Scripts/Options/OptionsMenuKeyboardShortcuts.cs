1a43f551586e1f60237e36173560a490765a4150
Giora Guttsait <giora111@gmail.com>
2018-08-16 00:46:30 +0300

    Add keyboard shortcuts to the options menu

---
+++ Assets/Scripts/Options/OptionsMenuKeyboardShortcuts.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenuKeyboardShortcuts : MonoBehaviour
+{
+    public OptionsMenu menu;
+    public Text optionsButtonText;
+
+    public KeyCode optionsKey = KeyCode.Return;
+
+    private void Start()
+    {
+        optionsButtonText.AddKeyboardShortcutText(optionsKey);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(optionsKey))
+            menu.Resume();
+    }
+}
