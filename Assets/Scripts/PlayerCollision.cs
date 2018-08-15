c044b77dd0bef78aaa3355a35e34cdb2fe3ef943
Giora Guttsait <giora111@gmail.com>
2018-08-16 02:02:21 +0300

    Add explosion effect when player hits obstacle

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    public PlayerMovement movement;
+    public PlayerMovement player;
+    public GameObject explosionEffect;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.collider.CompareTag(Constants.Tags.OBSTACLE))
             return;
 
+        DestroyPlayer();
         GameManager.Get().EndGame();
-        movement.StopPlayer();
+    }
+
+    private void DestroyPlayer()
+    {
+        player.StopPlayer();
+
+        FindObjectOfType<FollowPlayer>().enabled = false;
+        AudioManager.Instance.Play("ExplosionSound");
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
