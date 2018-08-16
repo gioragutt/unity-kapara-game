e3c710594f02cdb15a3726a2177c83800c9b7880
Giora Guttsait <giora111@gmail.com>
2018-08-16 22:42:30 +0300

    Add the pause menu (not yet openable from game)

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void Resume()
+    {
+        GameManager.Get().ResumeFromPauseMenu();
+    }
+
+    public void OpenOptions()
+    {
+        GameManager.Get().ShowOptionsMenu();
+    }
+
+    public void OpenStartMenu()
+    {
+        GameManager.Get().ShowStartMenu();
+    }
+}
---
+++ Assets/Scripts/Menus/PauseMenuKeyboardShortcuts.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuKeyboardShortcuts : MonoBehaviour
+{
+    public PauseMenu pauseMenu;
+    public Text resumeButtonText;
+    public Text optionsButtonText;
+    public Text startMenuButtonText;
+
+    public KeyCode resumeKey = KeyCode.Escape;
+    public KeyCode optionsKey = KeyCode.O;
+    public KeyCode startMenuKey = KeyCode.M;
+
+    private void Start()
+    {
+        resumeButtonText.AddKeyboardShortcutText(resumeKey);
+        optionsButtonText.AddKeyboardShortcutText(optionsKey);
+        startMenuButtonText.AddKeyboardShortcutText(startMenuKey);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(resumeKey))
+            pauseMenu.Resume();
+        if (Input.GetKey(optionsKey))
+            pauseMenu.OpenOptions();
+        if (Input.GetKey(startMenuKey))
+            pauseMenu.OpenStartMenu();
+    }
+}
