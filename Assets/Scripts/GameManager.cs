d30aa55cc1ee175514712004bf7656fa82515edc
Giora Guttsait <giora111@gmail.com>
2018-08-24 14:14:42 +0300

    Extract scene loading logic outside of GameManager to SceneLoader
    SceneLoader, like GameData, lives as a singleton object, that persists through levels
    
    related to #15

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,5 @@
 ﻿using Assets.Scripts;
-using Assets.Scripts.Menus;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -18,8 +15,6 @@ public class GameManager : MonoBehaviour
     public float endOfGameDelay = 3f;
     public GameObject completeLevelUi;
     public bool shouldBuildLevel = true;
-
-    private static readonly Stack<string> activeScenes = new Stack<string>();
 
     public event EventHandler GameEnded;
     public bool GameHasEnded
@@ -42,22 +37,22 @@ public class GameManager : MonoBehaviour
 
     public void ShowPauseMenu()
     {
-        StartCoroutine(OpenAdditiveScene(PauseMenuSceneName));
+        SceneLoader.Instance.OpenAdditiveScene(PauseMenuSceneName);
     }
 
     public void ResumeFromPauseMenu()
     {
-        CloseAdditiveScene(PauseMenuSceneName);
+        SceneLoader.Instance.CloseAdditiveScene(PauseMenuSceneName);
     }
 
     public void ShowOptionsMenu()
     {
-        StartCoroutine(OpenAdditiveScene(OptionsMenuSceneName));
+        SceneLoader.Instance.OpenAdditiveScene(OptionsMenuSceneName);
     }
 
     public void ResumeFromOptions()
     {
-        CloseAdditiveScene(OptionsMenuSceneName);
+        SceneLoader.Instance.CloseAdditiveScene(OptionsMenuSceneName);
     }
 
     public void RestartAtCheckpoint()
@@ -99,6 +94,11 @@ public class GameManager : MonoBehaviour
         Invoke("ShowCredits", restartDelay);
     }
 
+    private void ShowCredits()
+    {
+        LoadScene(CreditsSceneName);
+    }
+
     public void RestartGame()
     {
         LoadScene(FirstLevelSceneName);
@@ -116,68 +116,7 @@ public class GameManager : MonoBehaviour
 
     #region Implementation
 
-    private IEnumerator OpenAdditiveScene(string sceneName)
-    {
-        Debug.LogFormat("OpenAdditiveScene(\"{0}\")", sceneName);
-        Time.timeScale = 0;
-        var current = SceneManager.GetActiveScene();
-        ToggleKeyboardShortcuts(current);
-        activeScenes.Push(current.name);
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
-        yield return null; // wait for scene to load
-        var nextScene = SceneManager.GetSceneByName(sceneName);
-        SceneManager.SetActiveScene(nextScene);
-    }
-
-    private void ToggleKeyboardShortcuts(Scene scene)
-    {
-        Debug.Log("Toggling keys for scene: \"" + scene.name + "\"");
-        foreach (var go in scene.GetRootGameObjects())
-        {
-            var shortcuts = go.GetComponentsInChildren<MenuShortcuts>();
-            if (shortcuts.Length == 0)
-            {
-                continue;
-            }
-
-            Debug.Log("-- GameObject: \"" + go.name + "\"");
-            Array.ForEach(shortcuts, ToggleComponentEnabled);
-        }
-    }
-
-    private static void ToggleComponentEnabled(MonoBehaviour component)
-    {
-        var nextValue = !component.enabled;
-        Debug.LogFormat(
-            "Toggling {0} ({1} -> {2})",
-            component.name, 
-            component.enabled, 
-            nextValue);
-        component.enabled = nextValue;
-    }
-
-    private void CloseAdditiveScene(string sceneName)
-    {
-        if (activeScenes.Count == 0)
-        {
-            Debug.LogWarningFormat("CloseAdditiveScene(\"{0}\") called without opening it first", sceneName);
-            return;
-        };
-
-        Debug.LogFormat("CloseAdditiveScene(\"{0}\")", sceneName);
-        SceneManager.UnloadSceneAsync(sceneName);
-        var previousScene = SceneManager.GetSceneByName(activeScenes.Pop());
-
-        if (activeScenes.Count == 0)
-        {
-            Time.timeScale = 1f;
-        }
-
-        ToggleKeyboardShortcuts(previousScene);
-        SceneManager.SetActiveScene(previousScene);
-    }
-
-    private void SaveCheckpoint(int nextSceneBuildIndex)
+    private static void SaveCheckpoint(int nextSceneBuildIndex)
     {
         if (GameData.Instance == null)
         {
@@ -190,17 +129,13 @@ public class GameManager : MonoBehaviour
 
     private void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
-        var newScene = SceneManager.GetSceneByName(sceneName);
-        Debug.Log("Loaded Scene: " + newScene.name);
+        SceneLoader.Instance.LoadScene(sceneName);
         GameHasEnded = false;
     }
 
     private void LoadScene(int buildIndex)
     {
-        SceneManager.LoadScene(buildIndex);
-        var newScene = SceneManager.GetSceneByBuildIndex(buildIndex);
-        Debug.Log("Loaded Scene: " + newScene.name);
+        SceneLoader.Instance.LoadScene(buildIndex);
         GameHasEnded = false;
     }
 
@@ -215,11 +150,6 @@ public class GameManager : MonoBehaviour
         {
             return SceneManager.GetActiveScene().buildIndex + 1;
         }
-    }
-
-    private void ShowCredits()
-    {
-        LoadScene(CreditsSceneName);
     }
 
     private void OnGameEnded()
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,120 @@
+﻿using Assets.Scripts.Menus;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        private readonly Stack<string> activeScenes = new Stack<string>();
+
+        #region Implementation
+
+        public void OpenAdditiveScene(string sceneName)
+        {
+            StartCoroutine(OpenAdditiveSceneCoroutine(sceneName));
+        }
+
+        public void CloseAdditiveScene(string sceneName)
+        {
+            if (activeScenes.Count == 0)
+            {
+                Debug.LogWarningFormat("CloseAdditiveScene(\"{0}\") called without opening it first", sceneName);
+                return;
+            };
+
+            Debug.LogFormat("CloseAdditiveScene(\"{0}\")", sceneName);
+            SceneManager.UnloadSceneAsync(sceneName);
+            var previousScene = SceneManager.GetSceneByName(activeScenes.Pop());
+
+            if (activeScenes.Count == 0)
+            {
+                Time.timeScale = 1f;
+            }
+
+            ToggleKeyboardShortcuts(previousScene);
+            SceneManager.SetActiveScene(previousScene);
+        }
+
+        private IEnumerator OpenAdditiveSceneCoroutine(string sceneName)
+        {
+            Debug.LogFormat("OpenAdditiveScene(\"{0}\")", sceneName);
+            Time.timeScale = 0;
+            var current = SceneManager.GetActiveScene();
+            ToggleKeyboardShortcuts(current);
+            activeScenes.Push(current.name);
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            yield return new WaitForEndOfFrame(); // wait for scene to load
+            var nextScene = SceneManager.GetSceneByName(sceneName);
+            SceneManager.SetActiveScene(nextScene);
+        }
+
+        private void ToggleKeyboardShortcuts(Scene scene)
+        {
+            Debug.Log("Toggling keys for scene: \"" + scene.name + "\"");
+            foreach (var go in scene.GetRootGameObjects())
+            {
+                var shortcuts = go.GetComponentsInChildren<MenuShortcuts>();
+                if (shortcuts.Length == 0)
+                {
+                    continue;
+                }
+
+                Debug.Log("-- GameObject: \"" + go.name + "\"");
+                Array.ForEach(shortcuts, ToggleComponentEnabled);
+            }
+        }
+
+        private static void ToggleComponentEnabled(MonoBehaviour component)
+        {
+            var nextValue = !component.enabled;
+            Debug.LogFormat(
+                "Toggling {0} ({1} -> {2})",
+                component.name,
+                component.enabled,
+                nextValue);
+            component.enabled = nextValue;
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+            var newScene = SceneManager.GetSceneByName(sceneName);
+            Debug.Log("Loaded Scene: " + newScene.name);
+        }
+
+        public void LoadScene(int buildIndex)
+        {
+            SceneManager.LoadScene(buildIndex);
+            var newScene = SceneManager.GetSceneByBuildIndex(buildIndex);
+            Debug.Log("Loaded Scene: " + newScene.name);
+        }
+
+        #endregion Implementation
+
+        #region Singleton Component Implementation
+
+        public static SceneLoader Instance
+        {
+            get; private set;
+        }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        #endregion Singleton Component Implementation
+    }
+}
